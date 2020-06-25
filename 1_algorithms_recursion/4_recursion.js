33211854e44e7a07c5cb3bf5ff5baeaae5501962
Dave <dave.gagnat@gmail.com>
2020-06-25 03:29:55 -0500

    Added recursive notes

--- 1_algorithms_recursion/4_recursion.js
+++ 1_algorithms_recursion/4_recursion.js
@@ -1,5 +1,14 @@
 /*
+Anatomy of a Recursive Function
+  In recursive functions, we need to implement a way to stop the recursive loop
+  and prevent it from looping forever. We took care of the infinite loop issue in
+  our 'countDown' by using an if statement that prevents another recursive call.
+  In general, we call such a statement the BASE CASE
 
+  A recursive function consists of two fundamental parts:
+    -> The BASE CASE where we halt the recursion by bot making a further call
+    -> The RECURSIVE STEP where we continue the recursion by making another 
+      subsequent call
 
 
 */
