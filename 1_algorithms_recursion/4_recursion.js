11111c377103757bfae78aa58eb3c3713ef4b695
Dave <dave.gagnat@gmail.com>
2020-06-28 08:14:03 -0500

    Added to notes

--- 1_algorithms_recursion/4_recursion.js
+++ 1_algorithms_recursion/4_recursion.js
@@ -11,4 +11,9 @@ Anatomy of a Recursive Function
       subsequent call
 
 
+Factorial: Write a method 'factorial(n)' which takes a number and returns the 
+factorial of n. A factorial is the product of all whole numbers between 1 and n,
+inclusive. For example, 'factorial(5)' is 5 * 4 * 3 * 2 * 1 = 120.
+
+
 */
