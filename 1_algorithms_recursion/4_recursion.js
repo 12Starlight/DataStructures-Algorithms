76c31aa8b47f364ef3f5a4a603346858707d053a
Dave <dave.gagnat@gmail.com>
2020-06-28 08:27:15 -0500

    Updated recursion code snippets with notes

--- 1_algorithms_recursion/4_recursion.js
+++ 1_algorithms_recursion/4_recursion.js
@@ -15,5 +15,54 @@ Factorial: Write a method 'factorial(n)' which takes a number and returns the
 factorial of n. A factorial is the product of all whole numbers between 1 and n,
 inclusive. For example, 'factorial(5)' is 5 * 4 * 3 * 2 * 1 = 120.
 
+If we lay out the math used to calculate the factorial of some numbers, we will
+notice a pattern:
+
+factorial(5) = 5 * 4 * 3 * 2 * 1
+factorial(4) = 4 * 3 * 2 * 1
+factorial(3) = 3 * 2 * 1
+factorial(2) = 2 * 1
+factorial(1) = 1 (base case)
+
+
+The same pattern, just programmatically:
+
+factorial(5) = 5 * factorial(4)
+factorial(4) = 4 * factorial(3)
+factorial(3) = 3 * factorial(2)
+factorial(2) = 2 * factorial(1)
+factorial(1) = 1 (base case)
+
+Or in general, if 'n' is some number:
+  -> factorial(n) = n * factorial(n - 1)
+  -> factorial(1) = 1
+
+*/
+
+// Implemented
+const factorial = (n) => {
+  if (n === 1) return 1; // factorial(0) = 1, so this could be used instead
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // 120
+
+
+/*
+Solving a Problem Recursively:
+  Because every recursive problem must have a base and recursive case, we can 
+  follow these steps to help us write a recursive method:
+
+  1.) Identify the base case in the problem and code it. The base case should
+  explicitly handle the scenario(s) where the arguments are so trivially 'small'
+  that we immediately know the result without further calculation. Be sure it 
+  works by testing it. 
+
+  2.) Solve the next level of the problem, using the result of the base case. 
+  Test it.
+
+  3.) Modify the code in step 2, generalizing it for every level of the problem.
+
 
 */
