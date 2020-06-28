d9121cc6577ffd4bfe2fcddeb5ff8778ec69b9fc
Dave <dave.gagnat@gmail.com>
2020-06-28 10:22:23 -0500

    Solved reverseString(str)

--- 1_algorithms_recursion/recursion_project/lib/problems.js
+++ 1_algorithms_recursion/recursion_project/lib/problems.js
@@ -61,7 +61,9 @@ function sumArray(array) {
 // reverseString("internet")    // => "tenretni"
 // reverseString("friends")     // => "sdneirf"
 function reverseString(str) {
+ if (str.length === 0) return '';
 
+ return str[str.length - 1] + reverseString(str.slice(0, str.length - 1));
 }
 
 
