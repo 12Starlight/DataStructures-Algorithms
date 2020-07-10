5f865363d39a0c6fc5a450c322453a721b219c1a
Dave <dave.gagnat@gmail.com>
2020-07-10 12:18:12 -0500

    Reviewed solutions for minChange, minChangeMemo, leetCode-change()

--- 2_dynamic_programming_memoization/memoization_project/lib/problems.js
+++ 2_dynamic_programming_memoization/memoization_project/lib/problems.js
@@ -38,14 +38,14 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
-const minChange = (coins, amount) => {
+const minChangeBrute = (coins, amount) => {
     // console.log(amount);
     if (amount === 0) return 0;
 
     let numCoins = [];
     coins.forEach((coin) => {
         if (coin <= amount) {
-            numCoins.push(minChange(coins, amount - coin) + 1);
+            numCoins.push(minChangeBrute(coins, amount - coin) + 1);
         }
     });
 
@@ -53,26 +53,27 @@ const minChange = (coins, amount) => {
     return Math.min(...numCoins);
 }
 
-console.log(minChange([1, 2, 5], 11));
+console.log(minChangeBrute([1, 2, 5], 11));
 
 
-// const minChangeMemo = (coins, amount, memo = {}) => {
-//     if (amount === 0) return 0;
-//     if (amount in memo) return memo[amount];q
+const minChange = (coins, amount, memo = {}) => {
+    if (amount === 0) return 0;
+    if (amount in memo) return memo[amount];
 
-//     let numCoins = [];
-//     coins.forEach(coin => {
-//         if (coin <= amount) {
-//             numCoins.push(minChange(coins, amount - coin, memo) + 1 );
-//         }
-//     })
+    let numCoins = [];
+    coins.forEach(coin => {
+        if (coin <= amount) {
+            numCoins.push(minChange(coins, amount - coin, memo) + 1 );
+        }
+    })
 
-//     memo[amount] = Math.min(...numCoins);
-//     return memo[amount];
-// }
+    memo[amount] = Math.min(...numCoins);
+    return memo[amount];
+}
 
 
 module.exports = {
     lucasNumberMemo,   
+    minChangeBrute,
     minChange
 }; 
