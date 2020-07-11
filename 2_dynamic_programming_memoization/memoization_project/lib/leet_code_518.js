32f0994a35f4f33ad32073c1d539ca3b39f8df13
Dave <dave.gagnat@gmail.com>
2020-07-10 22:15:40 -0500

    Updated leetcode problem with notes

--- 2_dynamic_programming_memoization/memoization_project/lib/leet_code_518.js
+++ 2_dynamic_programming_memoization/memoization_project/lib/leet_code_518.js
@@ -2,14 +2,17 @@
 // Feel free to use this file for scratch work.
 
 const change = (amount, coins, memo = {}) => {
-  let key = amount + '-' + coins;
+  // remember coins also gets reduced, so it needs to be included in the key
+  let key = amount + '-' + coins; 
   if (key in memo) return memo[key];
   if (amount === 0) return 1;
 
-  let currentCoin = coins[coins.length - 1];
+  let currentCoin = coins[coins.length - 1]; // could start at beginning
 
   let total = 0;
+  // qty * currentCoin <= amount makes sure not to be negative
   for (let qty = 0; qty * currentCoin <= amount; qty++) {
+    // coins.slice(0, -1) reduces the coins array by 1 length
     total += change(amount - qty * currentCoin, coins.slice(0, -1), memo);
   }
 
