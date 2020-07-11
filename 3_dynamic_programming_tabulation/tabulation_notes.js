63a0b512af9053e3d963feeee53230f497cdeee0
Dave <dave.gagnat@gmail.com>
2020-07-11 09:24:59 -0500

    Built tabulationFib and refactoredTabFib() with notes

--- 3_dynamic_programming_tabulation/tabulation_notes.js
+++ 3_dynamic_programming_tabulation/tabulation_notes.js
@@ -35,3 +35,59 @@ const tabulatedFib = (n) => {
 }
 
 console.log(tabulatedFib(7)); // 13
+
+/*
+The length of the table is roughly 'n' elements long, so our algorithm will have
+an O(n) runtime. The space by our algorithm is also O(n) due to the size of the
+table. 
+
+*/
+
+
+/*
+Aside: Refactoring for O(1) space
+  We can cut down on the space used by our function. At any point of our loop we 
+  really only need the previous two subproblems, so there is little utility to 
+  storing the full array. This refactor is easy to do by using two variables:
+
+*/
+
+// Tabulated Fib: Refactored
+const refactoredTabFib = (n) => {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let secondLast = 0;
+  let last = 1;
+
+  for (let i = 2; i <= n; i++) {
+    let temp = last;
+    last += secondLast;
+    secondLast = temp;
+  }
+
+  return last;
+}
+
+console.log(refactoredTabFib(7)); // 13
+
+
+/*
+The Tabulation Formula:
+  Bear in mind that Tabulation or Memoization can only be applied to problems 
+  that can be divided into many subproblems of similar structure.
+
+  1.) Create the table array based off of the size of the input
+    -> this is not always straightforwar, if you have multiple args
+
+  2.) Initialize some values in the table that 'answer' the trivially small
+  subproblem
+    -> usually this means initializing the first entry of the table
+
+  3.) Iterate through the array and fill in remaining entries
+    -> calculating the next entry should require using other entries of the 
+    table
+
+  4.) Your final answer is the last entry in the table (usually)
+
+*/
