38fa4a3205bf996412c595ef4fd0ed8fcb146a90
Dave <dave.gagnat@gmail.com>
2020-07-15 13:47:51 -0500

    Completed Radix Sort, Radix Sort Negatives, with Time Complexity notes

--- 5_algorithms_efficient_sorts/radix_sort_notes.js
+++ 5_algorithms_efficient_sorts/radix_sort_notes.js
@@ -52,3 +52,79 @@ const getMaxDigits = (nums) => {
   }
   return maxDigits;
 }
+
+// Radix Sort: Input arrays containing only positive integers
+const radixSort = (arr) => {
+  if (!Array.isArray(arr)) return null;
+
+  let maxDigits = getMaxDigits(arr);
+  for (let k = 0; k < maxDigits; k++) {
+    let buckets = Array.from({length: 10}, () => []); // Array of empty arrays
+
+    for (let i = 0; i < arr.length; i++) {
+      let digit = getDigitForm(arr[i], k);
+      buckets[digit].push(arr[i]);
+    }
+
+    arr = [].concat(...buckets);
+  }
+
+  return arr;
+}
+
+// Radix Sort: Input arrays are both postive and negative integers
+const radixSortWithNegatives = (arr) => {
+  if (!Array.isArray(arr)) return null;
+
+  var negatives = arr.filter(item => item < 0);
+  var negativesSorted = [];
+  if (negatives.length > 0) {
+    negativesSorted = radixSort(negatives.map(item => Math.abs(item)))
+      .reverse()
+      .map(item => -item);
+  }
+
+  var positives = arr.filter(item => item >= 0);
+  let maxDigits = getMaxDigits(positives);
+
+  for (let k = 0; k < maxDigits; k++) {
+    let buckets = Array.from({length: 10}, () => []);
+
+    for (let i = 0; i < positives.length; i++) {
+      let digit = getDigitForm(positives[i], k);
+      buckets[digit].push(positives[i]);
+    }
+
+    positives = [].concat(...buckets);
+  }
+
+  return negativesSorted.concat(positives); 
+}
+
+/*
+Time Complexity: 
+  In general, the best, average, and worst case time complexities of Radix Sort 
+  are all the same.
+
+  Since the algorithm requires iterating over all 'n' elements of the input 
+  array, and doing so 'k' times, where 'k' is the length (number of digits) of
+  the longest integer in the array, we wind up with a run time of O(n * k). This 
+  makes Radix Sort faster than any of the previous comparison-based algorithms
+  we have seen earlier in the course!
+
+  However, there is actually some debate in the CS community over this topic. 
+  There exists a camp that believes that when Radix Sort's input array contains
+  entirely unique values, due to a characteristic of the way computers store 
+  numerical data, that 'k' becomes 'log k'. 
+  
+  If this is true, the absolute worst case scenario becomes the case where the
+  length of the longest integer in the input array, 'k', is equal to (or
+  greater than) the total number of elements in the array, 'n'. In this scenario,
+  we wind up with an O(n log(k)), or approximately O(n log(n)), run time, making
+  Radix Sort, at worst, equal in speed to our fastest comparison-based sorting
+  algorithm.
+
+  Though it will require some additional research, this may be a worthwhile 
+  talking point in an interview setting!
+
+*/
