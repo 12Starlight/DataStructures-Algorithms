943747112ffebb07f1e13007ca0663763a67f582
Dave <dave.gagnat@gmail.com>
2020-07-16 20:43:30 -0500

    Finished Counting Sort

--- 5_algorithms_efficient_sorts/counting_sort_notes.js
+++ 5_algorithms_efficient_sorts/counting_sort_notes.js
@@ -16,4 +16,48 @@ Why Integer Data?
   storage device for us to count the number of occurences of each integer in our
   input array. (Thus, the name 'Counting Sort'.)
 
+NOTE: This strategy will only work for input arrays containing exclusively
+positive integers. However we could do negatives with just a little extra 
+preliminary information - the smallest integer in the input array - we can 
+allocate 'largest - smallest' (where 'smallest is negative) elements to our 
+counter array upon initialization, remembering that our counter array's indices
+now represent values that are offset by 'Math.abs(smallest)'
+
+*/
+
+const countingSort = (arr, max) => {
+  const result = [];
+  const counters = new Array(max + 1).fill(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    counters[arr[i]]++;
+  }
+
+  for (let i = 0; i < counters.length; i++) {
+    while (counters[i] > 0) {
+      result.push(i);
+      counters[i]--;
+    }
+  }
+
+  return result;
+}
+
+/*
+Time Complexity:
+  In general, the best, average, and worst case time complexities of Counting
+  Sort are all the same.
+
+  Since this algorithm requires iterating over all 'n' elements of the input 
+  array, and then subsequently iterating over our element of our counter array
+  (which has length 'k'), we wind up with a run time of O(n + k). This makes
+  Counting Sort faster than any of the previous comparison-based algorithms, and
+  also faster than Radix Sort!
+
+*/
+
+/*
+Space Complexity
+  The trade off for Counting Sort comes with its space complexity.
+
 */
