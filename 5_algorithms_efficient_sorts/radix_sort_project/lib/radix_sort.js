aadb5ded6fc9d03908df16ead6ccd68d83fa192c
Dave <dave.gagnat@gmail.com>
2020-07-16 21:57:12 -0500

    Completed Radix Sort project

--- 5_algorithms_efficient_sorts/radix_sort_project/lib/radix_sort.js
+++ 5_algorithms_efficient_sorts/radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,35 @@
-function radixSort(arr) {
+// Build Helper Functions
+const getDigitForm = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
 
+const getIntLength = (num) => (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+
+const getMaxDigits = (nums) => {
+    let maxDigits = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+    }
+
+    return maxDigits;
+}
+
+
+const radixSort = (arr) => {
+    if (!Array.isArray(arr)) return null;
+
+    let maxDigits = getMaxDigits(arr);
+    for (let k = 0; k < maxDigits; k++) {
+        let buckets = Array.from({length: 10}, () => []); // Array of empty arrays
+
+        for (let i = 0; i < arr.length; i++) {
+            let digit = getDigitForm(arr[i], k);
+            buckets[digit].push(arr[i]);
+        }
+
+        arr = [].concat(...buckets);
+    }
+
+    return arr;
 }
 
 module.exports = {
