d64d32474135ed4d229f5dc4e409a3f9538080ae
Dave <dave.gagnat@gmail.com>
2020-07-18 14:14:18 -0500

    Completed Binary Search project

--- 6_algorithms_binary_search/binary_search_project/lib/binary_search.js
+++ 6_algorithms_binary_search/binary_search_project/lib/binary_search.js
@@ -1,10 +1,35 @@
-function binarySearch(array, target) {
+const binarySearch = (array, target) => {
+    if (array.length === 0) return false;
 
+    let midIdx = Math.floor(array.length / 2);
+    let leftHalf = array.slice(0, midIdx);
+    let rightHalf = array.slice(midIdx + 1);
+
+    if (target < array[midIdx]) {
+        return binarySearch(leftHalf, target);
+    } else if (target > array[midIdx]) {
+        return binarySearch(rightHalf, target);
+    } else {
+        return true;
+    }
 }
 
-function binarySearchIndex(array, target) {
+// The array that we consider goes from array.slice(low, high + 1);
+const binarySearchIndex = (array, target, low = 0, high = array.length - 1) => {
+    if (low === high) return -1;
 
+    let midIdx = Math.floor((low + high) / 2);
+
+    if (target < array[midIdx]) {
+        return binarySearchIndex(array, target, low, midIdx);
+    } else if (target > array[midIdx]) {
+        return binarySearchIndex(array, target, midIdx + 1, high);
+    } else {
+        return midIdx;
+    }
 }
+
+console.log(binarySearchIndex([5, 10, 12, 15, 20, 30, 70], 12, 1, 1));
 
 
 module.exports = {
