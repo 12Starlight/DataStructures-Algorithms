1340adc374216980306385ceef1118eba4b15bce
Dave <dave.gagnat@gmail.com>
2020-07-18 19:01:43 -0500

    Finished Stack with method notes

--- 7_data_structures_stacks_queues/stacks_queues_notes.js
+++ 7_data_structures_stacks_queues/stacks_queues_notes.js
@@ -101,3 +101,64 @@ value       The actual value this node represents
 next        The next node in the Stack (relative to this node)
 
 */
+
+/*
+Stack Methods
+  Insertion ~ push - Adds a Node to the top of the Stack
+    returns Integer - New size of stack
+
+  Deletion ~ pop - Removes a Node from the top of the Stack
+    returns Node removed from top of Stack
+
+  Meta ~ size - Returns the current size of the stack
+    returns Integer
+
+*/
+
+// Stack JavaScript Implementation
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.top) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const temp = this.top;
+      this.top = newNode;
+      this.top.next = temp;
+    }
+
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.top) return null;
+
+    const temp = this.top;
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+
+    this.top = this.top.next;
+    this.length--;
+    return temp.value;
+  }
+
+  size() {
+    return this.length; 
+  }
+}
