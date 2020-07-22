6d9fbdf92413f4f2e3ac2a828f298316b1e04770
Dave <dave.gagnat@gmail.com>
2020-07-22 15:17:12 -0500

    Solved treeHeight

--- 8_data_structures_trees/depth_breadth_first_project/lib/tree_height.js
+++ 8_data_structures_trees/depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,8 @@
-function treeHeight(root) {
+const treeHeight = (root) => {
+    if (!root) return -1;
 
+    // add 1 to include root
+    return 1 + Math.max(treeHeight(root.left), treeHeight(root.right));
 }
 
 
