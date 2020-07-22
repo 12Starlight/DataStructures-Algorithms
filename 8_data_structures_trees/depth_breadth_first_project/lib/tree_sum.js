b331cdc4299d1532a3417fb25840657f5096e07c
Dave <dave.gagnat@gmail.com>
2020-07-22 14:54:30 -0500

    Built treeSum with recursion

--- 8_data_structures_trees/depth_breadth_first_project/lib/tree_sum.js
+++ 8_data_structures_trees/depth_breadth_first_project/lib/tree_sum.js
@@ -1,8 +1,30 @@
-function treeSum(root) {
+const treeSum = (root) => {
+    if (!root) return 0;
+    let sum = 0;
+    let queue = [root];
 
+    while (queue.length) {
+        let node = queue.shift();
+
+        sum += node.val;
+
+        // remember to visit left first
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    return sum;
+}
+
+const treeSumRec = (root) => {
+    if (!root) return 0;
+
+    return treeSumRec(root.left) + root.val + treeSumRec(root.right);
 }
 
 
+
 module.exports = {
-    treeSum
+    treeSum,
+    treeSumRec
 };
