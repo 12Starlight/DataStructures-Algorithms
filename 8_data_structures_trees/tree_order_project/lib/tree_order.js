3a47be877ccee92db35b4ed8cae1df37d2f772dc
Dave <dave.gagnat@gmail.com>
2020-07-22 14:07:35 -0500

    Solved tree order project problems

--- 8_data_structures_trees/tree_order_project/lib/tree_order.js
+++ 8_data_structures_trees/tree_order_project/lib/tree_order.js
@@ -1,9 +1,21 @@
-function inOrderArray(root) {
+const inOrderArray = (root) => {
+    if (!root) return [];
 
+    return [
+        ...inOrderArray(root.left),
+        root.val,
+        ...inOrderArray(root.right)
+    ];
 }
 
-function postOrderArray(root) {
+const postOrderArray = (root) => {
+    if (!root) return [];
 
+    return [
+        ...postOrderArray(root.left),
+        ...postOrderArray(root.right),
+        root.val
+    ];
 }
 
 
