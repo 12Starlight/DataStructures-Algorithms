ae1593b4e210b10d4b09469ab71454ee691d5672
Dave <dave.gagnat@gmail.com>
2020-07-23 13:49:57 -0500

    Solved LeetCode 108

--- 9_data_structures_binary_search_trees/bst_project/lib/leet_code_108.js
+++ 9_data_structures_binary_search_trees/bst_project/lib/leet_code_108.js
@@ -2,6 +2,14 @@
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 
-function sortedArrayToBST(nums) {
+const sortedArrayToBST = (nums) => {
+  if (!nums.length) return null;
 
-}
+  let midIdx = Math.floor(nums.length / 2);
+  let root = new TreeNode(nums[midIdx]);
+  let leftSubtree = sortedArrayToBST(nums.slice(0, midIdx));
+  let rightSubtree = sortedArrayToBST(nums.slice(midIdx + 1));
+  root.left = leftSubtree;
+  root.right = rightSubtree;
+  return root;
+};
