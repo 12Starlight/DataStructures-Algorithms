b8f624a5ac4b87f0e2f8f585ec3b3490282676e5
Dave <dave.gagnat@gmail.com>
2020-07-22 21:46:49 -0500

    Tested Binary Search & Balance notes BST#search

--- 9_data_structures_binary_search_trees/binary_search_balance_notes.js
+++ 9_data_structures_binary_search_trees/binary_search_balance_notes.js
@@ -110,6 +110,18 @@ class BST {
   }
 }
 
+let tree1 = new BST();
+tree1.insert(10);
+tree1.insert(5);
+tree1.insert(16);
+tree1.insert(1);
+tree1.insert(7);
+tree1.insert(16);
+
+console.log(tree1.search(10));
+console.log(tree1.search(7));
+console.log(tree1.search(14));
+tree1.inOrderPrint();
 
 /*
 Height Balance:
