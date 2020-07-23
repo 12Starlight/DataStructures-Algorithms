ce7e8160d0e2566de50dd6bd34f0119b8d882fa1
Dave <dave.gagnat@gmail.com>
2020-07-23 13:33:59 -0500

    Solved LeetCode 110

--- 9_data_structures_binary_search_trees/bst_project/lib/leet_code_110.js
+++ 9_data_structures_binary_search_trees/bst_project/lib/leet_code_110.js
@@ -2,6 +2,15 @@
 //  https://leetcode.com/problems/balanced-binary-tree/
 
 
-function isBalanced(root) {
+const getHeight = root => {
+  if (!root) return -1;
 
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
 }
+
+const isBalanced = (root) => {
+  if (!root) return true;
+
+  let heightDiff = Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1;
+  return heightDiff && isBalanced(root.left) && isBalanced(root.right);
+};
