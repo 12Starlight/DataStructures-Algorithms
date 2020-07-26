3ba77a293eae96480150d67595f119765c68c960
Dave <dave.gagnat@gmail.com>
2020-07-26 16:27:03 -0500

    Completed class MaxHeap w constructor, getParent(idx), getLeftChild(idx), getRightChild(idx), insert(val), siftUp(idx), deleteMax(), and siftDown(idx)

--- 11_data_structures_heaps/heaps_project/lib/max_heap.js
+++ 11_data_structures_heaps/heaps_project/lib/max_heap.js
@@ -28,8 +28,40 @@ class MaxHeap {
             [ this.array[parent], this.array[idx] ] = [ this.array[idx], this.array[parent] ];
             this.siftUp(parent);
         }
+    } 
+
+    deleteMax() {
+        if (this.array.length === 2) return this.array.pop();
+        if (this.array.length === 1) return null;
+        
+        let max = this.array[1];
+        this.array[1] = this.array.pop();
+        this.siftDown(1);
+
+        return max;
     }
 
+    siftDown(idx) {
+        let arr = this.array;
+        let leftIdx = this.getLeftChild(idx);
+        let rightIdx = this.getRightChild(idx);
+        let leftVal = arr[leftIdx];
+        let rightVal = arr[rightIdx];
+
+        if (leftVal === undefined) leftVal = -Infinity;
+        if (rightVal === undefined) rightVal = -Infinity;
+
+        if (arr[idx] > rightVal && arr[idx] > leftVal) return;
+
+        let swapIdx;
+        if (rightVal > leftVal) {
+            swapIdx = rightIdx;
+        } else {
+            swapIdx = leftIdx
+        }
+        [ arr[idx], arr[swapIdx] ] = [ arr[swapIdx], arr[idx] ];
+        this.siftDown(swapIdx);
+    }
 }
 
 module.exports = {
