50b9e936dfbb193b0eff5d23b8f470595ff59028
Dave <dave.gagnat@gmail.com>
2020-07-26 15:50:26 -0500

    Built class MaxHeap w constructor, getParent, getLeftChild, getRightChild

--- 11_data_structures_heaps/heaps_project/lib/max_heap.js
+++ 11_data_structures_heaps/heaps_project/lib/max_heap.js
@@ -1,5 +1,19 @@
 class MaxHeap {
-    
+    constructor() {
+        this.array = [null];
+    }
+
+    getParent(idx) {
+        return Math.floor(idx / 2);
+    }
+
+    getLeftChild(idx) {
+        return idx * 2;
+    }
+
+    getRightChild(idx) {
+        return idx * 2 + 1;
+    }
 }
 
 module.exports = {
