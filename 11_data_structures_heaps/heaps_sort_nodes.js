2e3550f0a3ee20c920a0374e14455a68a9273596
Dave <dave.gagnat@gmail.com>
2020-07-26 19:04:53 -0500

    Started Heap Sort code snippets with notes

---
+++ 11_data_structures_heaps/heaps_sort_nodes.js
@@ -0,0 +1,31 @@
+/*
+Heap Sort:
+  We have emphasized heavily thata heaps are a 'partially ordered' data 
+  structure. However, we can still leverage heaps in a sorting algorithm to end
+  up with a fully sorted array. The strategy is simple using our previous
+  'MaxHeep' implementation:
+    
+    1.) Build the heap: 'insert' all elemnets of the array into a 'maxHeap'
+    
+    2.) Construct the sorted list: continue to 'deleteMax' until the heap is
+    empty, every deletion will return the next element in decreasing order
+
+
+    The code is straightforward:
+
+*/
+
+// assuming our 'MaxHeap' from the previous section
+const heapSort = (array) {
+  // Step 1: build the heap
+  let heap = new MaxHeap();
+  array.forEach(num => heap.insert(num));
+
+  // Step 2: constructed the sorted array
+  let sorted = [];
+  while (heap.array.length > 1) {
+    sorted.push(heap.deleteMax());
+  }
+
+  return sorted;
+}
