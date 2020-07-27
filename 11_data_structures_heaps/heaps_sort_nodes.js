4398e13599002b62b6e7d29b54101cd765940978
Dave <dave.gagnat@gmail.com>
2020-07-26 19:15:48 -0500

    Added Space & Time Complexity for Heap Sort

--- 11_data_structures_heaps/heaps_sort_nodes.js
+++ 11_data_structures_heaps/heaps_sort_nodes.js
@@ -34,3 +34,24 @@ const heapSort = (array) => {
 
 let arr = [1,2,4,3,8,19,29,28,400];
 console.log(heapSort(arr));
+
+/*
+Time Complexity Analysis: O(nlog(n))
+  -> 'n' is the size of the input array
+  -> step-1 requires 'O(n)' time as previously discussed
+  -> step-2's while loop requires 'n' steps in isolation and each 'deleteMax'
+  will require 'log(n)' steps to restore max heap property 
+  (due to sifting-down). This means step 2 costs 'O(nlog(n))'
+  -> the total time complexity of the algorithm is 'O(n + nlog(n)) = O(nlog(n));
+
+*/
+
+/*
+Space Complexity Analysis:
+  So 'heapSort' performs as fast as our other efficient sorting algorithms, but
+  how does it fair in space complexity? Our implementation above requires an
+  extra 'O(n)' amount of space bc the heap is maintained separately from the
+  input array. If we can figure out a way to do all of these heap operations in
+  place we get constant 'O(1)' space! Let us work on this now.
+  
+*/
