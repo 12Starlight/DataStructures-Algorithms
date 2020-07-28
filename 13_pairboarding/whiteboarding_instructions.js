4c7e04a568b02e83c6b69218f29fcf236d258c99
Dave <dave.gagnat@gmail.com>
2020-07-28 15:31:04 -0500

    Added whiteboarding instructions

---
+++ 13_pairboarding/whiteboarding_instructions.js
@@ -0,0 +1,29 @@
+/*
+Whiteboarding Mock Interview Instructions:
+  The following is a section containing a bank of problems to be used to conduct 
+  mock interviews.
+
+  Pair assignments change each day. One in the pair takes a question set A, the
+  other takes question set B. The interviewer has a few interview questions, 
+  plus one coding problem and its solution. He/she makes the other student, the
+  interviewee, solve the problem as if they were doing a real interview. The
+  interviewer cannot give out an actual answer, but should keep the interviewee
+  on track with leading questions. The interviewee cannot ask for a hint or
+  answer, but may ask insightful questions. Practice at least some of the
+  problems on a whiteboard or on paper, it is what you will be given during
+  in-person interviews.
+
+  Give yourself time before starting, to make sure you understand the problem
+  you will give your interviewee as well as its solution. Keep in mind that 
+  multiple solutions are likely valid.
+
+  You may not refer to the web unless the question assums technical knowledge
+  that neither the interviewer nor the interviewee has (eg. the nature of a
+  certain data structure). When the interviewee has found a solution, he or she
+  should determine its time complexity and try to optimize.
+
+  Finally, switch the roles with a new question set. If you are with an odd
+  number of people, one group of three can be composed of two interviewers and
+  an interviewee.
+
+*/
